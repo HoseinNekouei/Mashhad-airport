f7585535477074fa956e40c3ba2dbcb54ff5f676
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-09-27 12:58:08 +0330

    Implemented VectorStorCache class based on chromaDb

--- app.py
+++ app.py
@@ -134,6 +134,68 @@ def split_documents_into_chunks(documents: List[Any]) -> List[str]:
     return chunks
 
 
+#---------------------VECTOR STORE ------------------------------
+class VectorStoreCache:
+    """ ChromaDB-based Vector Store cache"""
+
+    def __init__(self, persist_dir='./Chroma_Store'):
+        self.persist_dir = persist_dir
+        self.embedding_model= load_embeddings()
+        self.collection = None
+
+    async def load_or_create(self, text_chunks: List[str]):
+
+        # Load Existing ChromaDB or create a new with only missing chunks
+        if os.path.exists(self.persist_dir) and os.listdir((self.persist_dir)):
+            #load existing collection
+            self.collection= Chroma(
+                embedding_function= self.embedding_model,
+                persist_directory= self.persist_dir
+            )
+
+        else:
+            try:
+            #create a new collection
+                self.collection= await asyncio.to_thread(
+                    Chroma.from_texts,
+                    texts= text_chunks,
+                    embedding=self.embedding_model,
+                    persist_directory=self.persist_dir
+                    )
+                                
+                return self.collection
+                     
+            except Exception as e:
+                st.error(f"Unexpected error while creating ChromaDB collection: {e}")
+                st.stop()
+
+        # check with chunks are missing
+        existing_ids= set(self.collection.get()['ids'])
+
+        # Filter out chunks that already exist
+        new_chunks, new_ids= [],[]
+        for chunk in text_chunks:
+            chunk_id= hash_text(chunk)
+            if chunk_id not in existing_ids:
+                new_chunks.append(chunk)
+                new_ids.append(chunk_id)
+
+        if new_chunks:
+            await asyncio.to_thread(
+                self.collection.add_texts,
+                ids = new_ids,
+                texts= new_chunks
+                )
+
+
+    async def similarity_search(self, query: str, k=4):
+
+        if not self.collection:
+            raise ValueError('Vecto store not loaded. call create_or_load() first')            
+
+        return await asyncio.to_thread(self.collection.similarity_search, query, k=k)
+    
+
 if __name__== '__main__':
     print(load_embeddings())
 
